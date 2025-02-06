ad78a103e0fff171dea7a213acacc80aeef5d6b4
AHMEDabdamine <ahmedabdesselam55@gmail.com>
2025-02-06 15:21:39 +0100

    first commitment

---
+++ README.md
@@ -0,0 +1,6 @@
+### Dz SELL website 
+
+https://ahmedabdamine.github.io/dzsell/
+
+
+<img src="assets/images/logo/logo.svg" alt="Logo" />
---
+++ assets/js/language .js
@@ -0,0 +1,273 @@
+let lang = {
+    ar: { 
+        demo:"النسخة التجريبية",
+        home: "الصفحة الرئيسية",
+        about : "حولنا ",
+        price: "التسعيرة",
+        tm: "الفريق",
+        contact: "تواصل معنا " , 
+        doc : "الوثيقة" , 
+        sell : "برنامج نقاط البيع الوحيد الذي تحتاجه  ", 
+        dzsell: " , DG POS " ,
+        desc : " ⚡ برنامج نقاط البيع المستند على الويب والذي يمكن تشغيله على إلكترون وفي متصفحك " , 
+        Learn : "اكتشف المزيد",
+        download :"حمله الآن ",
+        fr :"الخصائص",
+        feature: " الخصائص الرئيسية لـ DG POS " ,
+        feat: "عزز المبيعات الخاصة بمشرروعك وتحكم فيها بسهولة مع الخصائص العديدة التي يقدمها لك DG POS",
+        ui : "واجهة استخدام  سهلة وبسيطة توفر لك تجربة رائعة ",
+        uidesc:"برنامج نقاط البيع الخاص بنا بسيط للغاية وسهل الاستخدام ومناسب للمبتدئين. كما أنه يدعم العديد من اللغات. يتضمن أيضًا لوحة تحكم وقسم للتقارير لتتبع حالة عملك.",
+        instdesc:"نحن نوفر لكم خدمة تثبيت وإعداد البرنامج مجانًا",
+        inst:"دعم مجاني للتثبيت والإعداد ",
+        mail:"إرسال الإيصالات عبر البريد الإلكتروني ",
+        maildesc:"برنامج DG POS مجهز ليمكن المستخدم أو البائع من إرسال إيصال البيع أو الفاتورات عبر البريد الإلكتروني الخاص بالعميل ",
+        easy:"سهولة الوصول والإدارة ",
+        easydesc:"برنامج DG POS  يسهل عملة الوصول له للمستخدمين من خلال إمكانية مشاركته على الشبكة ، وإحتواؤه على نظام تنظيم الموضفين وصلاحياتهم يجعل إدارة مشاريعكم سهلة ",
+        aboutdzsell:"حول DG POS ",
+        aboutttl:"برنامج ذكي يساعدك على تسيير وتحسين متجرك  ",
+        p1 : "تصور برنامجًا لحلول برمجي يُنظم عملية تشغيل عملك بالكامل. ستصبح إدارة المخزون سهلة للغاية مع ميزات مثل التحكم في المخزون لكل عنصر ومجموعة، وكلها قابلة للتخصيص مع مجموعة واسعة من السمات. يمكنك التعامل مع حالات ضريبة القيمة المضافة والضريبة على السلع والخدمات (GST) والضرائب متعددة المستويات بسهولة باستخدام الدعم المدمج.",
+        p2 : "أدر مبيعاتك دون جهد باستخدام سجل نقاط البيع الذي يسجل كل معاملة. قم بإنشاء عروض أسعار وفواتير احترافية، كل ذلك مع تتبع المصروفات بسهولة. هل تحتاج إلى إجراء عملية صرف نقدي؟ البرنامج يوفر لك ذلك. كما أنه يسمح بالطباعة وإرسال إيصالات الفواتير والعروض الأسعار مباشرة عبر البريد الإلكتروني. وبالإضافة إلى ذلك، فإن هذا البرنامج الرائع", 
+        pricee:" عروضنا مريحة لجيبك المسكين لذا لا تقلق 😊",
+        pricettl:"عروضنا ",
+        start:"تبدأ من ",
+        buy:"اشتري الآن ",
+        POPULAR:"الأكثر شعبية  ",
+        dzselll:"برنامج DG POS ",
+        pc:"كمبيوتر",
+        scanner:"قارئ باركود",
+        printer:"طابعة حرارية ",
+        buynow:"تواصل معنا الآن",
+        ask:"أيّة أسئلة ؟ ",
+        commonask: "أكثر الأسئلة طرحا من طرف زبائننا وإجاباتها ",
+        1:"كيف لي أن أنزل وأثبت DG POS ",
+        a1:"بعد إقتنائك لـ DG POS حمل تطبيق rustdesk  وتواصل معنا سنعمل على تجهيز كل شيء من أجلك أو يمكنك الإطلاع على الوثيقة إذا أردت إعداد كل شيء بمفردك",
+        2:"ماهي إجابيات استعمال DG POS ؟",
+        a2:"تكمن إيحابيات استعمال برنامج إدارة البيع الخاص بنا في إبقاء تجارتكم منظمة وأنيقة مع الخصائص العديد لاتباع الأرباح والخسائر وتنظيم المتاجر",
+        3:"هل يدعم DG POS  استيراد وتصدير المعلومات عن طريق  أكسل ؟ ",
+        a3:" شيء أكيد فبرنامجنا مجهز بتقنية استيراد وتصدير المعلومات كالزبائن والسلع وغير ذلك وكذلك يدعم صيغ عديدة عند تصدير المعلومات كـ PDF و xlsx و sql    ",
+        4:"هل يتوافق DG POS مع مختلف أنواع الماسحات الضوئية والطابعات ",
+        a4:"نعم ، نظام DG POS يتوافق مع معظم أو يمكن القول جميع أنواع الماسحات الضوئية والطابعات الحبرية منها أو الحرارية .",
+        5:"كيف يمكنني ضبط DG POS ليتوافق مع ميزان الباركود ؟",
+        a5:"يمكن ضبط هذا من خلال الإعدادات الخاصة بالباركود حيث يمكنك اختيار نوع الباركود المطبوع من طرف الميزان الذكي وبعدها إنشاء صيغة إدخال جديدة . تم شرح هذا أيضا في الوثيقة ودليل الإستعمال الخاص بـئ DG POS ",
+        6 :"ماذا يتسعمل DG POS كقاعدة بيانات ؟", 
+        a6:"يستند DG POS على قاعد بيانات MYSQL التي يتم توفيرها من طرف برنامج WAMP server , تتمتع هذه الأخيرة بعدة خصائص مثل النسخ الإحتياطي لقاعدة البيانات وغيرها ", 
+        meettm:"قابل الفريق ", 
+        hamed:"عبد السلام أحمد ",
+        dev : "مطور ويب  ", 
+        sp:" الدعم", 
+        terms :"سياسة الخدمة ",    
+        Refund  :" سياسة الإسترجاع",
+        ft:"DG POS يمنحك تجربة سلسة في إدارة مشروعك ونشاطك التجاري",
+        pre1:"   1- المتطلبات اللازمة لتشغيل DG POS  ",
+        pre:"  المتطلبات اللازمة لتشغيل DG POS  ",
+
+           downloadd:"حمّل",
+           wmap:"- حزمة تثبيت WAMP server",
+           c:"- حزمة تثبيت جميع متطلبات ويندوز (حزمات C++ )", 
+           dalil:"دليلك في DG POS",
+           daliil :"دليل تثبيت DG POS ",
+           dzs:"2- اقتني DG POS وحمله ",
+           or1:"قم بشراء DG POS من موقعنا أو ",
+           or2:"وتحصل على رابط التحميل .",
+           Extract:"3 - فك الضغط عن المتطلبات ",
+           ect:"فك  الضغط عن محتوى الملف المحمل سابقا  سيصبح لديك ملفين ",
+           cinst:"4 - تثبيت حزمة C++ ",
+           isn:"استخرج الحزمة  وابحث عن ملف  'install_all.bat'. قم بتشغيل هذا الملف وانتظر حتى يكتمل التثبيت. وافق على جميع المطالبات التي تظهر أثناء التثبيت. لا تقلق إذا رأيت موجه أوامر، فهذا أمر طبيعي.",
+           wampinst:" 5- تثبيت WAMP server ",
+           wampp:"قم بتشغيل Wamp server.exe واتبع الخطوات التالية في تثبيته :",
+            s1:"اختر لغتك وانقر على 'موافق'",
+            s2:"وافق على شروط الاستخدام وانقر على'التالي'",
+            s3:"اختر الموقع الذي تريد حفظ ملفات WAMP فيه. تذكر هذا الموقع وانقر على'التالي'",
+            s4:"استمر في النقر على'التالي' حتى يتم تثبيت خادم WAMP",
+            Instdzssel:"6- تثبيت  DG POS ",
+            inst1:"قم بتشغيل الملف DG POS.exe قم بالمصادقة على الاذن بالضغط على Ok ثم سيظهر المثبت قم بالضغط على   التالي ، التالي ... حتى تصل الى الجزء الخاص بالملفات قم بتثبيته في ملف www الخاص بـ  Wamp server مثال : C:/wamp64/www/Dzsell وبعدها اتمم بالنقر على التالي ",
+            startwmp:"7- قم بتشغيل WAMP server  ",
+            wampgd:"اتجه نحو الايقونة الخضراء ذات الحرف W فكل العمل سيتم هناك ",
+            php:"8- إعداد وتجهيز php ",
+            phpcon:"اضغط على الأيقونة الخضراء وقم بالتوجه نحو php version واختار الإصدار رقم 7.4.33",
+            phpadm:"9- الوصول إلى phpmyadmin",
+            phpadmin:"اضغط على الأيقونة الخضراء ثم اتجه نحو phpmyadmin وانقر عليها ، بعدها سيتم فتح نافذة جديدة في المتصفح .",
+            dbc:"10- إنشاء قاعدة البيانات ",
+            db:"قم بتسجيل الدخول إلى phpmyadmin بوضع 'root ' كاسم المستخدم مع ابقاء كلمة السر فارغة ثم توجه نحو قواعد البيانات وقم بإنشاء قاعدة بيانات جديدة باسم dzsell ",
+            dbimp:"11- استيراد قاعدة البيانات ",
+            imp:"اضغط على 'import' ثم 'Browse'. انتقل إلى هذا المسار: C:\wamp64\www\DG POS\databases\dzsell.sql. قم بالتمرير لأسفل وانقر على''import'. انتظر بضع ثوان ، سترى رسالة تأكيد.",
+            virtual:"12- إنشاء الإستضافة الافتراضية",
+       grW:"توجه نحو الأيقونة الخضراء الخاصة بالسيرفر وإنقر على local host ثم اختر add a virtualhost",
+       
+            
+            vh:"13- إعداد الإستضافة الافتراضية ",
+
+            f1:"املأ الإدخال الأول بأي عبارة تريد ك www.dzsell.com أو pos او dzsell",
+            f2:"قم بنسخ مكان تخزين  DG POS وملاأ الإدخال الثاني مثال : C:/wamp64/www/DzSELL  ",
+            f3:"اترك الإدخال الثالث فارغا ",
+            f4:"قم بالضغط على start the creation of the virtual host وأعد تشغيل wamp server", 
+            final:"14- البدأ في استعمال DG POS ",
+            fn:"تم إعداد DG POS الآن للاستخدام! قم بتشغيل التطبيق الخاص بك وسجّل الدخول باستخدام المعلومات التي قدمناها لك وانتظر حتى يكتمل نقل قاعدة البيانات. يمكنك الآن البدء في جني الأموال من خلاله  ومتابعة عملك .",
+       talk:"لنتكلم حول DG POS ",
+       love:"نحن نحب الإستماع إلى رأيك 😊",
+       location:"موقعنا ",
+       loc:"المدية - الجزائر ",
+       help:"تواصل معنا لمساعدك عبر البريد ",
+       dwn:"التحميل ",
+       dzdwn:"حمل DG POS الآن ",
+       clickdwn:"اضغط على زر التحميل للتحميل",
+       access:"يمكنكم شراء DG POS من خلال الضغط على زر التحميل ثم Request access سيتم التواصل معكم من قبل الفريق واتمام عملية الدفع  يمكنكم أيضا ترك رقم هاتفكم و أي وسيلة تواصل  في الخانة المخصصة لارسال الرسالة  ",
+       downloadserver :"حمّلmariaDB SQL server  ",
+       
+       
+        },
+    en: {
+        home: " Home",
+        about : "About ",
+        price: "pricing",
+        tm: "Team",
+        contact: "Contact" , 
+        doc : "Documentation"  , 
+        sell : "Made in Ghana for Ghana  ", 
+        desc : "great POS for your awesome SHOP 😊",
+        Learn : "Learn More",
+        download :"Download Now ",
+        fr :"Features",
+        feature: "Main Features of DG POS " ,
+        feat: " Supercharge your sales with our feature-rich POS system. All the POS features you need to thrive, in one easy-to-use platform. ",
+        ui : " simple UI and great user experience ",
+        uidesc:"our pos softwate is really simple and easy to use and beginner friendly it also Support many languages. also it include a dashboard and a reports section to track the statue of your business",
+        instdesc:" we provide  installation service for our Customers ",
+        inst:"installation support ",
+        mail :"E-mail Sending",
+        maildesc:"our POS software can handle sending invoices and reciept to your customers ",
+        easy:"Easy to access",
+        easydesc:" the web structure of our software make it able to run on browser or the included app , also you can access it with another pc from the local network ",
+        aboutdgpos:" About DG POS ",
+        aboutttl:"Brilliant software that can improve your business",
+        p1 : " Imagine a software solution that streamlines your entire business operation. Inventory management becomes a breeze with features like stock control for individual items and kits, all customizable with a wide range of attributes. Handle complex tax situations with built-in VAT, GST, and multi-tier taxation support. ",
+        p2: " Manage your sales effortlessly with a point-of-sale register that logs every transaction. Generate professional quotes and invoices, all while keeping track of expenses with ease. Need to perform a cash up? The software has you covered. It even allows for printing and emailing receipts, invoices, and quotes directly. ",
+        pricee:" Our pricing plans are designed to fit your needs. and not damage you financially. ",
+        pricettl:"Our Pricing Plans",
+        start:" STRARTING FROM ",
+        buy:"Purchase Now",
+
+        POPULAR:"POPULAR",
+        dgpos:" DG POS ",
+        pc:"computer",
+        scanner:"Barcode Scanner ",
+        printer:"Thermal Printer ",
+        buynow:"Contact us NOW",
+        ask:"Any Questions? ",
+        1:"How can i install it ?",
+        a1:"after purchusing  the software download Rustdesk and contact us we will setup everthing for you",        
+        commonask:" cummon asked Questions from our customers ",
+    dgposl:"DG POS it self",
+        2:"what are the benifits i can get by using DG POS ?",
+        a2:"our POS software help too keep your bisness orgnised and help track your bisness statue with the built in dashboard and reports feature.",
+        3:" Does DG POS support MS Excel when importing data or exporting it ?",
+        a3:" sure thing , Our POS software is able to handle data importing and exporting with MS excel or any woorsheet and also can export data in many formats like PDF , SQL , xlsx ...",
+        4:"does DG POS copatible with diffrent brands of barcode scanners and printers ?",
+        a4:" Yes , DG POS is compatible with ALL brands of barcode scanners and thermal printers",
+        6 :"what does DG POS use as a database ?", 
+        a6:"DG POS use mysql as a database wich is provided by the WAMP server . ", 
+        meettm:"meet the team ",
+        hamed:"Ahmed Abdesselam",
+        
+        dev :"Full stack devlopper",
+        sp:" support", 
+        terms :"Terms of service",
+        Refund:"Refund policy",
+
+        ft:" we give you a soft experience managenig your business.",
+        pre1:" 1- Download the Prerequisites ",
+        pre:"the required files from our website. These will include the following:",
+
+           downloadd:"Download",
+           wmap:" - Wamp server installer ",
+           c:" - windows C++ packages ",  
+           dalil:"DG POS Guide",
+           daliil :"DG POS Installation Guide 🚀⚡⚡",
+           dzs:"2. Purchase DG POS and Download",
+           or1:"Purchase DG POS from our website or",
+           or2:"d obtain the download link. Download the DG POS package",
+           Extract:"3. Extract the Prerequisites",
+           ect:"Extract the contents of the downloaded 'prerequisites.zip' file. You will find two files:",
+           cinst:"4. Install Microsoft C++ Package",
+           isn:"Extract the 'visual studio C++ pack' and locate the 'install_all.bat' file. Execute this file and wait for the installation to complete. Accept all prompts during the installation. Don't worry if you see a command prompt, this is normal. ",
+           wampinst:"5. Install WAMP Server",
+           wampp:"Install the WAMP server by executing the downloaded .exe installer. Follow these steps:",
+           s1:"Choose your language and click 'OK'.",
+           s2:"Accept the terms of usage and click 'Next'.",
+           s3:"Choose the location where you want to save the WAMP files. Remember this location and click 'Next'.",
+           s4:"Continue clicking 'Next' until the WAMP server is installed.",
+           Instdzssel:"6- Install DG POS",
+           inst1:" installer. A window will pop up. Click 'OK'. The installer will prompt you for a location to save the DG POS files. Install it in the 'www' folder of your WAMP server (e.g., C:\wamp64\www\DG POS). If you are installing it on the C drive, you can just click 'Next' as the default path should be correct ",
+           startwmp:"7. Start WAMP Server",
+            wampgd:"Go to the WAMP shortcut you just installed and run it. Wait for all services to start. You will see a green 'W' icon on your taskbar.",
+            php:"9. Access phpMyAdmin",
+            phpcon:"Click on the green 'W' icon on the taskbar. Navigate to 'PHP' and change the PHP version to 7.3. The server will restart automatically.",
+            phpadm:"9- Access phpMyAdmin",
+            phpadmin:"Click on the green 'W' icon again. Navigate to 'phpMyAdmin' and run it. A new tab will open in your web browser and phpMyAdmin will start.",
+            dbc:"10- Create the DG POS Database",
+            db:"Enter the username ''root'' (without a password). In the ''Databases'' section, click ''New'' to create a new database. Name it ''dgpos''. This will create a database named ''dgpos''.",
+            dbimp:"11- Import the Database",
+            imp:"Click on 'Import' and then 'Browse'. Go to this path: C:\wamp64\www\DG POS\databases\dzsell.sql. Scroll down and click 'Import'. Wait for a few seconds, and you will see a confirmation message.",
+            vh:"12- Create a Virtual Host",
+            fild:"Go back to the green 'W' icon on the taskbar. Navigate to 'localhost' (or type 'localhost' in your browser). This will open a page with localhost controls. Click 'Add New Virtual Host'.",
+            f1:"**First Input:** Enter 'www.dzsell.com' or any other desired name (use a short name like ''dzsell''or''pos'').",
+            f2:"**Second Field:** Enter the path where you installed DG POS (e.g., C:\wamp64\www\DG POS). ",
+            f3:"**Third Field:** Leave this empty.",
+            f4:"Click 'Create' to create the virtual host. Restart the WAMP server by right-clicking the green 'W' icon and selecting 'Restart all services'.",             // :"",
+      
+        final:"14- Start Using DG POS",
+        fn:"DG POS is now ready to use! lanch your app login with the inforamtions provided by us and wait for the database migration to finish . You can start making money with it 🤑 and track you business 🚀🚀.",
+        talk:"Let’s talk about DG POS",
+        love:"Love to hear from you!",
+        location:"Our location ",
+        loc:"Medea - Algeria ",
+        help:"how can we help you ?",
+     dwn:"download",
+            dzdwn:"Download DG POS ",
+                 
+            clickdwn:"Click the button below to start downloading DG POS . ",
+        access:" request the access by clicking the Request access button and we will contact you back 😊 ",
+        downloadserver :"Download mariaDB server",
+
+    },
+   
+   
+    }
+
+
+;
+document.addEventListener("DOMContentLoaded", () => {
+    const languageSelector = document.getElementById('language-selector');
+    const body = document.body; 
+
+    // Set the language selector to the stored language or default to 'en'
+    const storedLang = localStorage.getItem('selectedLanguage') || 'en';
+    languageSelector.value = storedLang;
+    translatePage(storedLang);
+    setDirection(storedLang);
+
+    languageSelector.addEventListener('change', (event) => {
+        const selectedLang = event.target.value;
+        // Store the selected language in local storage
+        localStorage.setItem('selectedLanguage', selectedLang);
+        translatePage(selectedLang);
+        setDirection(selectedLang);
+    });
+
+    function setDirection(lang) {
+        if (lang === 'ar') {
+            body.classList.add('rtl');
+        } else {
+            body.classList.remove('rtl');
+        }
+    }
+});
+
+function translatePage(language) {
+    document.querySelectorAll('[data-lang]').forEach(element => {
+        const key = element.getAttribute('data-lang');
+        if (lang[language] && lang[language][key]) {
+            element.textContent = lang[language][key];
+        }
+    });
+}
